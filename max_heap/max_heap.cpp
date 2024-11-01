2c7057e4bd18d60642558ba26b812750fe3ba147
Jeong Hun <jhunlee56@naver.com>
2024-11-02 01:11:28 +0900

    숫자외에 다른 값들도 추가

--- max_heap/max_heap.cpp
+++ max_heap/max_heap.cpp
@@ -1,34 +1,54 @@
 #include <iostream>
 using namespace std;
+class TaskRecord {
+	string taskname;
+	string instructions;
+	string PersonWhoWilYellIfThisIsNotDone;
+	bool completed;
+public:
+	float priority;
+	TaskRecord() {
+		priority = 0;
+		taskname = "";
+		instructions = "";
+		PersonWhoWilYellIfThisIsNotDone = "";
+		completed = false;
+	}
+};
+
+bool isLessThan(TaskRecord a, TaskRecord b) {
+	return a.priority < b.priority;
+}
+
 class Heap {
-	int* array;
+	TaskRecord* array;
 	int array_size;		//배열의 크기
 	int last_index;		//마지막 인덱스
 public:
 	Heap(int size) {
-		array = new int[size];
+		array = new TaskRecord[size];
 		array_size = size;
 		last_index = 0;
 	}
 
-	void InsertHeap(int value) {
+	void InsertHeap(float value) {
 		if (last_index == array_size - 1) {	//크기검사
-			int* trr = array;
+			TaskRecord* trr = array;
 			array_size *= 2;
-			array = new int[array_size];	//배열 크기 두배로 늘리기
+			array = new TaskRecord[array_size];	//배열 크기 두배로 늘리기
 			for (int i = 1; i < array_size; i++) {
 				array[i] = trr[i];
 			}
-			delete trr;
+			delete[] trr;
 		}
 
 		last_index++;
-		array[last_index] = value;			//삽입
+		array[last_index].priority = value;			//삽입
 
 		int current = last_index;
 		int parent = last_index / 2;	//크기 비교 부분
-		while (1 <= parent && array[parent] < array[current]) {
-			int temp = array[parent];
+		while (1 <= parent && isLessThan(array[parent],array[current])){
+			TaskRecord temp = array[parent];
 			array[parent] = array[current];
 			array[current] = temp;
 
@@ -37,29 +57,29 @@ public:
 		}
 	}
 
-	int removeMaxHeap() {
+	TaskRecord removeMaxHeap() {
 		if (last_index == 0) {
-			return -1;
+			return TaskRecord();
 		}
 		//마지막과 루트를 교환하고 힙 크기 축소
-		int result = array[1];
+		TaskRecord result = array[1];
 		array[1] = array[last_index];
-		array[last_index] = 0;
+		array[last_index] = TaskRecord();
 		last_index--;
 		//루트를 올바른 위치로
 		int i = 1;
 		while (i <= last_index) {
 			int swap = i;
-			if (2 * i <= last_index && (array[swap] < array[2 * i])) {
+			if (2 * i <= last_index && isLessThan(array[swap] , array[2 * i])) {
 				swap = 2 * i;
 			}  
 			
-			if (2 * i + 1 <= last_index && (array[swap] < array[2 * i + 1])) {
+			if (2 * i + 1 <= last_index && isLessThan(array[swap] , array[2 * i + 1])) {
 				swap = 2 * i + 1;
 			}
 
 			if (i != swap) {
-				int temp = array[i];
+				TaskRecord temp = array[i];
 				array[i] = array[swap];
 				array[swap] = temp;
 				i = swap;
@@ -67,13 +87,13 @@ public:
 				break;
 			}
 
-			return result;
 		}
+		return result;
 	}
 
 	void show() {
 		for (int i = 1; i <= last_index; i++) {
-			cout << array[i] << endl;
+			cout << array[i].priority << endl;
 		}
 	}
 };
