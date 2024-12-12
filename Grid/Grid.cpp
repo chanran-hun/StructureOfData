14549e3d2b0bca8400053e6570f74998ca847f67
JEONG HUN LEE <jhunlee56@naver.com>
2024-12-12 22:17:25 +0900

    지속적으로 추가 중

--- Grid/Grid.cpp
+++ Grid/Grid.cpp
@@ -1,47 +1,63 @@
 #include <iostream>
-#include <cmath>	//floor 연산을 하기 위해서 필요한 라이브러리 입니다.
+#include <cmath>
 using namespace std;
+class GridPoint {
+	float x;	//x좌표
+	float y;	//y좌표
+
+public:
+	GridPoint* next;	//다음 점
+
+	GridPoint() {
+		x = 0;
+		y = 0;
+		next = nullptr;
+	}
+	GridPoint(float Xpoint, float Ypoint) {
+		x = Xpoint;
+		y = Ypoint;
+		next = nullptr;
+	}
+};
 
 class Grid {
 	int num_x_bins;	//x축 빈 상자 개수
 	int num_y_bins;	//y축 빈 상자 개수
-	float x_start;	//격자에 포함된 x의 최솟값
-	float x_end;	//격자에 포함된 x의 최댓값
-	float x_bin_width;
-	float y_start;	//격자에 포함된 y의 최솟값
-	float y_end;	//격자에 포함된 y의 최댓값
-	float y_bin_width;
+	float x_start, x_end, x_bin_width;
+	float y_start, y_end, y_bin_width;
 	GridPoint** bins; //저장한 좌표들
 
 public:
-	bool insertGrid(float x, float y) {
+	Grid(int x_bins, int y_bins, float x_s, float x_e, float y_s, float y_e) : num_x_bins(x_bins), num_y_bins(y_bins), x_start(x_s), 
+		x_end(x_e), y_start(y_s), y_end(y_e) {
+		x_bin_width = (x_end - x_start) / num_x_bins;
+		y_bin_width = (y_end - y_start) / num_y_bins;
+
+		bins = new GridPoint*[num_x_bins];  // 첫 번째 차원 (x축)
+		for (int i = 0; i < num_x_bins; i++) {
+			bins[i] = new GridPoint[num_y_bins]();  // 두 번째 차원 (y축)
+		}
+	}
+	~Grid() {
+		// 메모리 해제
+		for (int i = 0; i < num_x_bins; ++i) {
+			delete[] bins[i];
+		}
+		delete[] bins;
+	}
+
+	bool insertGrid(Grid* g, float x, float y) {
 		int xbin = floor((x - x_start) / x_bin_width);
 		int ybin = floor((y - y_start) / y_bin_width);
 
-		if (xbin < 0 || num_x_bins <= xbin) {
-			return false;
-		}
-		if (ybin < 0 || num_y_bins <= ybin) {
+		if (xbin < 0 || num_x_bins <= xbin || ybin < 0 || num_y_bins <= ybin) {
 			return false;
 		}
 
-		GridPoint next_point = bins[xbin][ybin];
+		GridPoint* next_point = bins[xbin][ybin];
 		bins[xbin][ybin] = GridPoint(x, y);
-		bins[xbin][ybin].next = &next_point;
+		bins[xbin][ybin].next = next_point;
 
 		return true;
-	}
-};
-
-class GridPoint {
-	float x;	//x좌표
-	float y;	//y좌표
-	
-public:
-	GridPoint* next;	//다음 점
-	GridPoint(float setX, float setY) {
-		x = setX;
-		y = setY;
-		next = nullptr;
 	}
 };
