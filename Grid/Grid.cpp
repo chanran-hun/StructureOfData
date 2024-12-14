38b00353819ae813258b89b8aef1562947a46f92
Jeong Hun <jhunlee56@naver.com>
2024-12-14 17:26:10 +0900

    근접 점 찾기 함수 추가

--- Grid/Grid.cpp
+++ Grid/Grid.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
-#include <cmath>	//floor 연산을 하기 위해서 필요한 라이브러리 입니다.
+#include <cmath>	//floor 연산, abs 연산을 하기 위해서 필요한 라이브러리 입니다.
+#define THRESHOLD 5.0
 using namespace std;
+
+bool approx_equal(float x1, float y1, float x2, float y2) {	//근처의 점을 찾기 위한 도우미 함수 입니다.
+	if (abs(x1 - x2) > THRESHOLD) {
+		return false;
+	}
+	if (abs(y1 - y2) > THRESHOLD) {
+		return false;
+	}
+	return true;
+}
 
 class Grid {
 	int num_x_bins;	//x축 빈 상자 개수
 	int num_y_bins;	//y축 빈 상자 개수
-	float x_start;	//격자에 포함된 x의 최솟값
-	float x_end;	//격자에 포함된 x의 최댓값
-	float x_bin_width;
-	float y_start;	//격자에 포함된 y의 최솟값
-	float y_end;	//격자에 포함된 y의 최댓값
-	float y_bin_width;
+	float x_start, x_end, x_bin_width;
+	float y_start, y_end, y_bin_width;
 	GridPoint** bins; //저장한 좌표들
 
 public:
@@ -18,16 +25,13 @@ public:
 		int xbin = floor((x - x_start) / x_bin_width);
 		int ybin = floor((y - y_start) / y_bin_width);
 
-		if (xbin < 0 || num_x_bins <= xbin) {
-			return false;
-		}
-		if (ybin < 0 || num_y_bins <= ybin) {
+		if (xbin < 0 || num_x_bins <= xbin || ybin < 0 || num_y_bins <= ybin) {
 			return false;
 		}
 
 		GridPoint next_point = bins[xbin][ybin];
 		bins[xbin][ybin] = GridPoint(x, y);
-		bins[xbin][ybin].next = &next_point;
+		bins[xbin][ybin].next = &next_point;	
 
 		return true;
 	}
